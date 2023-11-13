03da9a2587f9f7a0a30b5c2fa33369edfa59adfe
aajodar <aajodar@gmail.com>
2023-11-13 21:09:11 +0100

    pip instalable files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math-quiz',
+    version='0.1.0',
+    url='https://github.com/aajodar/DSSK_homework2.git',
+    author='Antonio Jodar Martin',
+    packages=find_packages(),
+    install_requires=[
+        #No external dependencies
+    ]
+)
